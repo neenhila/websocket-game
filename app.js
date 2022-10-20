708a2761d9340eab04a28cf17cac2ed032e425c2
Neenhila <83518175+neenhila@users.noreply.github.com>
2022-10-20 19:19:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.static(__dirname));
 let players = {};
 
 const checkUsernameExists = (username) => {
@@ -24,27 +25,24 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  let username = `User${Math.random()
-    .toFixed(7)
-    .toString()
-    .slice(2, 7)}`;
+  let username = `User${Math.random().toFixed(7).toString().slice(2, 7)}`;
   if (checkUsernameExists(username))
-    username = `User${Math.random()
-      .toFixed(9)
-      .toString()
-      .slice(4, 9)}`;
+    username = `User${Math.random().toFixed(9).toString().slice(4, 9)}`;
   players[socket.id] = {
     name: username,
   };
 
- 
   socket.on("setUsername", (details) => {
-      if(details.name !== null) return players[socket.id].name = details.name;
+    if (details.name !== null) {
+      players[socket.id].name = details.name;
+      return io.emit("players", players)
+    }
+    socket.emit("noUsername", { name: players[socket.id].name });
+  });
 
-      socket.emit("noUsername", {name: players[socket.id].name })
-  })
+  io.emit("players", players);
 });
 
-server.listen(process.env.PORT, () => {
+server.listen(3000, () => {
   console.log("listening on *:3000");
 });
