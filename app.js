02488c4ebdcc73c0972c245fee25972d545caba5
Neenhila <83518175+neenhila@users.noreply.github.com>
2022-10-20 18:53:11 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let players = {};
+
+const checkUsernameExists = (username) => {
+  let isUniq = true;
+  Array.from(players).forEach((player) => {
+    if (player.name === username) return (isUniq = false);
+  });
+  isUniq
+    ? console.log("Username is unique")
+    : console.log("Username was not unique");
+  return isUniq;
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  let username = `User${Math.random()
+    .toFixed(7)
+    .toString()
+    .slice(2, 7)}`;
+  if (checkUsernameExists(username))
+    username = `User${Math.random()
+      .toFixed(9)
+      .toString()
+      .slice(4, 9)}`;
+  players[socket.id] = {
+    name: username,
+  };
+
+ 
+  socket.on("setUsername", (details) => {
+      if(details.name !== null) return players[socket.id].name = details.name;
+
+      socket.emit("noUsername", {name: players[socket.id].name })
+  })
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
