9adab9ab726efbfefdd46ad27d04841a36b066de
Neenhila <83518175+neenhila@users.noreply.github.com>
2022-10-21 19:35:59 +0300

    Adding movement abilites.

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.use(express.static(__dirname));
-let players = {};
+let players = new Map();
 
-const checkUsernameExists = (username) => {
+const isUnique = (username) => {
   let isUniq = true;
-  Array.from(players).forEach((player) => {
-    if (player.name === username) return (isUniq = false);
-  });
-  isUniq
-    ? console.log("Username is unique")
-    : console.log("Username was not unique");
+  for (let player in players) {
+    console.log(player)
+    if (players[player].name === username) return (isUniq = false);
+  }
   return isUniq;
 };
 
@@ -26,21 +24,56 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("a user connected");
   let username = `User${Math.random().toFixed(7).toString().slice(2, 7)}`;
-  if (checkUsernameExists(username))
+  if (!isUnique(username))
     username = `User${Math.random().toFixed(9).toString().slice(4, 9)}`;
   players[socket.id] = {
     name: username,
+    x: 0,
+    y: 0,
+    active: false
   };
+  // all players location refresh on every 10ms
+  setInterval(() => {
+    io.emit("players", players);
+  }, 10);
 
   socket.on("setUsername", (details) => {
+    let username;
     if (details.name !== null) {
-      players[socket.id].name = details.name;
-      return io.emit("players", players)
+      username = details.name;
+      if (!isUnique(username))
+        username = `User${Math.random().toFixed(9).toString().slice(4, 9)}`;
+      players[socket.id].name = username;
     }
     socket.emit("noUsername", { name: players[socket.id].name });
+
+    players[socket.id].active = true;
   });
 
-  io.emit("players", players);
+  socket.on("right", (playerName) => {
+    for(let player in players){
+      player = players[player];
+      if(player.name === playerName) return player.x += 1;
+    }
+  });
+  socket.on("left", (playerName) => {
+    for(let player in players){
+      player = players[player];
+      if(player.name === playerName) return player.x -= 1;
+    }
+  });
+  socket.on("top", (playerName) => {
+    for(let player in players){
+      player = players[player];
+      if(player.name === playerName) return player.y -= 1;
+    }
+  });
+  socket.on("down", (playerName) => {
+    for(let player in players){
+      player = players[player];
+      if(player.name === playerName) return player.y += 1;
+    }
+  });
 });
 
 server.listen(3000, () => {
